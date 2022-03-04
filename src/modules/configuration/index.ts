4640c29e9e36cdee8afd0e6c7fc48fcb318ca2b2
Ryan <rshtirmer@gmail.com>
2022-03-04 15:45:41 -0500

    update config

--- src/modules/configuration/index.ts
+++ src/modules/configuration/index.ts
@@ -5,8 +5,10 @@ export default () => ({
   port: process.env.APP_PORT,
   app_env: process.env.APP_ENV,
   ethereum_network: process.env.ETHEREUM_NETWORK,
+  ethereum_quorum: process.env.ETHEREUM_QUORUM,
   infura: {
     project_id: process.env.INFURA_PROJECT_ID,
+    project_secret: process.env.INFURA_PROJECT_SECRET,
   },
   aws: {
     region: process.env.AWS_REGION,
